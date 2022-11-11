8cb0806e629075be8e4c69438893a93d281c6d09
isaiasca <1307759@senati.pe>
2022-11-11 18:42:25 -0500

    estilo de login

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/listar', function () {
+    return view('listar');
+});
+
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
